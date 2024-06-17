49b632bff4d18fbc8bb23c32d59b6eeedf450ff7
Zakosman0909 <60045232+Zakosman0909@users.noreply.github.com>
2024-06-17 19:51:46 -0400

    Plotted the graphs

---
+++ data_transforms.py
@@ -0,0 +1,89 @@
+import numpy as np
+import pandas as pd
+
+
+def fft_df(d):
+    return np.fft.fft(d)
+
+
+def time_to_frequency(df: pd.DataFrame):
+    df_fft = df.apply(fft_df)
+
+    # Real component
+    df_fft_real = pd.DataFrame(np.real(df_fft),
+                               index=df_fft.index,
+                               columns=df_fft.columns)
+
+    df_fft_real.iloc[0] = df_fft_real.iloc[1]
+
+    df_fft_imag = pd.DataFrame(np.imag(df_fft),
+                               index=df_fft.index,
+                               columns=df_fft.columns)
+
+    df_fft_imag.iloc[0] = df_fft_imag.iloc[1]
+    df_fft_mag = np.sqrt(np.square(df_fft_real).add(np.square(df_fft_imag)))
+
+    # Add _fft to the column names for future concat
+    col_names = [s + '_fft' for s in df_fft_mag.columns]
+    df_fft_mag.columns = col_names
+    df_fft_real.columns = col_names
+    df_fft_imag.columns = col_names
+
+    return df_fft_mag, df_fft_real, df_fft_imag
+
+
+def rolling_ranges(df: pd.DataFrame, min_window, max_window=None,
+                   measures=("mean", "var"), lag=1, dropna=True):
+    """
+    Calculate rolling window statistics for a range of window sizes.
+    Concatenate these with unique names to the input dataframe.
+    :param df: pd.Dataframe
+    :param min_window: starting window size
+    :param max_window: ending window size
+    :param measures: statistical measures to calculate (..., "min", "max")
+    :param lag: Shift to add to data (t-1, ...)
+    :param dropna: remove initial nan rows created by the rolling windows
+    :return: Concatenated original dataframe with all the rolling windows
+    """
+    window_size = 0
+
+    if max_window:
+        # Typical window sizes are odd
+        window_range = np.arange(start=min_window, stop=max_window + 1, step=2)
+    else:
+        window_range = [max_window]
+
+    df_concat = df
+    for window_size in window_range:
+        df_roll = df.shift(periods=lag).rolling(window=window_size).agg(measures)
+
+        # create discrete names based on window size
+        col_names = [(s + f"_wnd{window_size}", u) for s, u in df_roll.columns]
+        df_roll.columns = col_names
+
+        df_concat = pd.concat([df_concat,
+                               df_roll.set_axis(df_concat.index)], axis=1)
+
+    if dropna:
+        df_concat = df_concat.iloc[window_size:].reset_index(drop=True)
+
+    return df_concat
+
+
+def zmuv_norm_df(df):
+    """
+    Calculate zero-mean unit-variance shift for columns in dataframe
+    Constants are transformed to 0.0
+    Called with pd.apply(func)
+    :param df: passed by pandas
+    :return:
+    """
+
+    if df.std() == 0.:
+        return df - df.mean()
+    else:
+        return (df - df.mean()) / df.std()
+
+
+def minmax_norm_df(df: pd.DataFrame):
+    return (df - df.min()) / (df.max() - df.min())
--- plot_dataset.py
+++ plot_dataset.py
@@ -1,55 +1,148 @@
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
-#Dataframe for Pressure Sensors 
-dfp1 = pd.read_csv('PS1.txt' , sep='\t', header=None)
-dfp2 = pd.read_csv('PS2.txt' , sep='\t', header=None)
-dfp3 = pd.read_csv('PS3.txt' , sep='\t', header=None)
-dfp4 = pd.read_csv('PS4.txt' , sep='\t', header=None)
-dfp5 = pd.read_csv('PS5.txt' , sep='\t', header=None)
-dfp6 = pd.read_csv('PS6.txt' , sep='\t', header=None)
-
-#Dataframe for Temperature Sensors
-dft1 =  pd.read_csv('TS1.txt' , sep='\t', header=None)
-dft2 =  pd.read_csv('TS2.txt' , sep='\t', header=None)
-dft3 =  pd.read_csv('TS3.txt' , sep='\t', header=None)
-dft4 =  pd.read_csv('TS4.txt' , sep='\t', header=None)
+import glob
+import os
+import sys
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+  
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
+import data_transforms as dt
 
 
-#Dataframe for Motor Power Sensor
-dfe1 =  pd.read_csv('EPS1.txt' , sep='\t', header=None)
+def file_read(file_path):
+    return pd.read_csv(file_path, sep='\t', header=None)
 
-#Dataframe for Volume Flow Sensor
-dff1 = pd.read_csv('FS1.txt' , sep='\t', header=None)
-dff2 = pd.read_csv('FS2.txt' , sep='\t', header=None)
 
-#Dataframe for Vibration Sensor
-dfv1 = pd.read_csv('VS1.txt' , sep='\t', header=None)
+def main():
+    print("Running Test on Hydraulic Systems dataset")
+    
+    dataset_dir = os.path.abspath(os.path.dirname(__file__))
+    print(f"Dataset directory: {dataset_dir}") 
+    files = sorted(glob.glob(os.path.join(dataset_dir, "*.txt")))
 
-#Dataframe for Efficiecy Factor
-dfs1 = pd.read_csv('SE.txt' , sep='\t', header=None)
+    if not files:
+        sys.exit('No dataset found')
 
-#Dataframe for Virtual Cooling Efficiency Sensor
-dfce1 = pd.read_csv('CE.txt' , sep='\t', header=None)
+    data_dict = {}
+    for file_path in files:
+        file_name = os.path.basename(file_path)
+        print(f'Loading {file_name}...')
 
-#Dataframe for Virtual Cooling Power Sensor
-dfcp1 = pd.read_csv('CP.txt' , sep='\t', header=None)
+        if 'description' in file_name or 'documentation' in file_name or 'profile' in file_name:
+            continue
+        else:
+            data = file_read(file_path)
+            key = file_name.split('.')[0]
+            print(f'{key}, Shape: {data.shape}')
+            data_dict[key] = data
 
-#print(dfp1)
-#print(dft1)
-#print(dfe1)
-#print(dff1)
-#print(dfv1)
-#print(dfs1)
-#print(dfce1)
-#print(dfcp1)
+            print('Dataframe Info/Description')
+            print(data.info())
+            print(data.describe())
 
-#dfp1.iloc[0:10].plot()
-#dft1.iloc[0:10].plot()
-#dfe1.iloc[0:10].plot()
-#dff1.iloc[0:10].plot()
-#dfv1.iloc[0:10].plot()
-#dfs1.iloc[0:10].plot()
-#dfce1.iloc[0:10].plot()
-#dfcp1.iloc[0:10].plot()
-#plt.show()
+    ans_df = pd.read_csv(os.path.join(dataset_dir, 'profile.txt'), sep='\t', header=None,
+                         names=['Cooler', 'Valve', 'Pump', 'Accumulator', 'Stable'])
+
+    unstables = np.where(ans_df['Stable'].values == 0)[0]
+    stables = np.where(ans_df['Stable'].values == 1)[0]
+
+    num_keys = len(data_dict.keys())
+    cols = 4
+    rows = int(np.ceil(num_keys / cols))
+
+    num = 250
+    plt.figure(figsize=(20, rows * 4))
+    for i, key in enumerate(data_dict.keys()):
+        plt.subplot(rows, cols, i + 1)
+        plt.title(f'{key}', fontsize=6)
+        for v in stables[:num - 1]:
+            plt.plot(data_dict[key].iloc[v], color='green')
+        plt.plot(data_dict[key].iloc[stables[num]], label='stable', color='green')
+        for v in unstables[:num]:
+            plt.plot(data_dict[key].iloc[v], color='red', alpha=0.5)
+        plt.plot(data_dict[key].iloc[unstables[num]], label='unstable', color='red', alpha=0.5)
+        plt.legend(loc='upper right', fontsize=8)
+        plt.xticks(fontsize=6)
+        plt.yticks(fontsize=6)
+
+    plt.tight_layout(pad=3.0)
+    plt.show()
+
+    # Apply FFT transformation
+    plt.figure(figsize=(20, rows * 4))
+    for i, key in enumerate(data_dict.keys()):
+        plt.subplot(rows, cols, i + 1)
+        plt.title(f'{key} FFT', fontsize=6)
+        data_fft = data_dict[key].apply(dt.fft_df)
+        data_fft_real = pd.DataFrame(np.real(data_fft), index=data_fft.index, columns=data_fft.columns)
+        data_fft_real.iloc[0] = data_fft_real.iloc[1]
+        data_fft_imag = pd.DataFrame(np.imag(data_fft), index=data_fft.index, columns=data_fft.columns)
+        data_fft_imag.iloc[0] = data_fft_imag.iloc[1]
+        data_fft_mag = np.sqrt(np.square(data_fft_real).add(np.square(data_fft_imag)))
+        for v in stables[:num - 1]:
+            plt.plot(data_fft_mag.iloc[v], color='blue')
+        plt.plot(data_fft_mag.iloc[stables[num]], label='stable', color='blue')
+        for v in unstables[:num]:
+            plt.plot(data_fft_mag.iloc[v], color='orange', alpha=0.5)
+        plt.plot(data_fft_mag.iloc[unstables[num]], label='unstable', color='orange', alpha=0.5)
+        plt.legend(loc='upper right', fontsize=8)
+        plt.xticks(fontsize=6)
+        plt.yticks(fontsize=6)
+
+    plt.tight_layout(pad=3.0)
+    plt.show()
+
+    # Apply PCA transformation on the time domain
+    pca_max = min([len(data_dict[key].columns) for key in data_dict.keys()])  # Adjust pca_max to the minimum number of columns
+    plt.figure()
+    for key in data_dict.keys():
+        data = data_dict[key]
+        std_scaler = StandardScaler()
+        data_scaled = std_scaler.fit_transform(data)
+        pca_range = np.arange(start=1, stop=pca_max + 1)
+        var_ratio = []
+        for num in pca_range:
+            pca = PCA(n_components=num)
+            pca.fit(data_scaled)
+            var_ratio.append(np.sum(pca.explained_variance_ratio_))
+        plt.plot(pca_range, var_ratio, marker='o', label=f'{key}')
+    
+    plt.grid()
+    plt.xlabel('# Components')
+    plt.ylabel('Explained Variance Ratio')
+    plt.title('Time Domain PCA')
+    plt.legend(loc='lower right')
+    plt.show()
+
+    # Apply PCA transformation on the frequency domain
+    plt.figure()
+    for key in data_dict.keys():
+        data = data_dict[key]
+        data_fft = data.apply(dt.fft_df)
+        data_fft_real = pd.DataFrame(np.real(data_fft), index=data_fft.index, columns=data_fft.columns)
+        data_fft_real.iloc[0] = data_fft_real.iloc[1]
+        data_fft_imag = pd.DataFrame(np.imag(data_fft), index=data_fft.index, columns=data_fft.columns)
+        data_fft_imag.iloc[0] = data_fft_imag.iloc[1]
+        data_fft_mag = np.sqrt(np.square(data_fft_real).add(np.square(data_fft_imag)))
+        std_scaler = StandardScaler()
+        data_scaled = std_scaler.fit_transform(data_fft_mag)
+        pca_range = np.arange(start=1, stop=pca_max + 1)
+        var_ratio = []
+        for num in pca_range:
+            pca = PCA(n_components=num)
+            pca.fit(data_scaled)
+            var_ratio.append(np.sum(pca.explained_variance_ratio_))
+        plt.plot(pca_range, var_ratio, marker='o', label=f'{key}')
+    
+    plt.grid()
+    plt.xlabel('# Components')
+    plt.ylabel('Explained Variance Ratio')
+    plt.title('Frequency Domain PCA')
+    plt.legend(loc='lower right')
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
